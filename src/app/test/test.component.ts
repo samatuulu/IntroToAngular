0bbdcf8b174051ce88b706e02c44f918dcc5852e
Bektursun <samatuulu11@gmail.com>
2019-12-19 16:09:57 +0600

    available things that we can do in html with interpolation

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  template: '<h2> welcome {{ name }}</h2>',
+  template: `<h2> welcome {{ name }}</h2> <h2> {{ 2+2 }}</h2>
+  <h2>{{ "Welcome " +  name }}</h2>
+  <h2>{{ name.length }}</h2>
+  <h2>{{ name.toUpperCase() }}</h2>
+  <h2> {{ greetUser() }} </h2>`,
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
@@ -11,6 +15,10 @@ export class TestComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  greetUser(){
+    return "Hello " + this.name;
   }
 
 }
