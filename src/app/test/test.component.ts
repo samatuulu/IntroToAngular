2f3c9333de7867b2acde554c8a4b1b8160df98ac
Bektursun <samatuulu11@gmail.com>
2019-12-23 00:21:45 +0600

    ngIf Directive in Angular

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,21 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-  <input [(ngModel)]="name" type="text">
-  {{name}}
+  <div *ngIf="displayName; then thenBlock; else elseBlock"></div>
+
+  <ng-template #thenBlock>
+  <h2>Codevolution</h2>
+  </ng-template>
+
+  <ng-template #elseBlock>
+  <h2>Hidden</h2>                                                                             
+  </ng-template>
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
   public name = "";
+  public displayName = false;
  
   constructor() { }
 
   ngOnInit() {
-  }
-
-  logMessage(value){
-    console.log(value)
   }
 
 }
