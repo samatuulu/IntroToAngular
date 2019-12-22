320a91a00c39063dd1be326697a0bc96c90dc2c8
Bektursun <samatuulu11@gmail.com>
2019-12-22 17:38:35 +0600

    Two ways of Binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,18 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-  <h2>Welcome {{name}} </h2>
-  <input #myInput type="text">
-  <button (click)="logMessage(myInput.value)">Log</button>
-
-  
-
-  
+  <input [(ngModel)]="name" type="text">
+  {{name}}
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
-  public name = 'Bektursun'
+  public name = "";
  
   constructor() { }
 
