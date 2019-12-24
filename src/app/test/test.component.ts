a74583b88f37808001e99ab59ccbba47455c3db4
Bektursun <samatuulu11@gmail.com>
2019-12-24 23:46:36 +0600

    ngSwitch Directive in Angular

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,21 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-  <div *ngIf="displayName; then thenBlock; else elseBlock"></div>
-
-  <ng-template #thenBlock>
-  <h2>Codevolution</h2>
-  </ng-template>
-
-  <ng-template #elseBlock>
-  <h2>Hidden</h2>                                                                             
-  </ng-template>
+  <div [ngSwitch]="color">
+    <div *ngSwitchCase="'red'">You picked red color</div>
+    <div *ngSwitchCase="'blue'">You picked blue color</div>
+    <div *ngSwitchCase="'green'">You picked green color</div>
+    <div *ngSwitchDefault>Pick again!</div>
+  </div>
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
   public name = "";
-  public displayName = false;
+  public color = "orange"
  
   constructor() { }
 
